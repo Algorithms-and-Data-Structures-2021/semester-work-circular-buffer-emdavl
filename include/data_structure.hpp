1f4ed012a0f756d91e72120b13aa5ad5eecf132a
Emil Davlyatov <emil.davlyatov2014@yandex.ru>
2021-04-11 19:48:32 +0300

    Implemented some methods, tests required.

--- include/data_structure.hpp
+++ include/data_structure.hpp
@@ -1,27 +1,59 @@
 #pragma once
 
+#include "node.hpp"
+#include "linked_dequeue.hpp"
+
 // Заголовочный файл с объявлением структуры данных
 
 namespace itis {
 
   // Tip 1: объявите здесь необходимые структуры, функции, константы и прочее
 
-  // Пример: объявление константы времени компиляции в заголовочном файле
-  inline constexpr auto kStringConstant = "Hello, stranger!";
+  //inline constexpr auto kStringConstant = "Hello, stranger!";
 
   // Пример: объявление структуры с полями и методами
-  struct MyStructure {
-   public:
-    int size_{0};
+  struct Circular_buffer final {
+   private:
+    // Указатель на самый старый добавленный узел.
+    SinglyNode *oldest_node{nullptr};
+    // Указатель на последний добавленный узел
+    SinglyNode *newest_node{nullptr};
     int capacity_{0};
-    int* data_{nullptr};
+    int size_{0};
 
-    // Tip 2: На начальном этапе разработки структуры данных можете определения методов задавать в
-    // заголовочном файле, как только работа будет завершена, можно будет оставить здесь только объявления.
+   public:
+    //Вроде конструктор
+    explicit Circular_buffer(int capacity_arg);
 
-    int size() const {
-      return size_;
-    }
+    // Добавляет элемент на следующую, после предыдущего добавленного позицию. Если она занята самым старым элементом,
+    // то происходит перезаписывание.
+    void add(int e);
+
+    // Возвращает значение самого старого элемента, при этом элемент изымается из очереди и его место становится пустым;
+    int get();
+
+    // Возвращает значение самого старого элемента, при это не удаляя элемент из очереди
+    int peek();
+
+    // Очищает буфер
+    void clear();
+
+    // Возвращает true - если capacity==size, иначе false
+    bool isFull();
+
+    // Возвращает true - если capacity!=size, иначе false
+    bool isEmpty();
+
+    //Возвращает текущее количество элементов
+    int currentSize();
+
+    // Вспомогательный метод для добавления элементов, когда буфер заполнен
+    void addIfFull(int e);
+
+    // Вспомогательный методо для добавления элементов, когда буфер не заполнен
+    void addIfNotFull(int e);
+
+    ~Circular_buffer();
+
   };
-
 }  // namespace itis
--- src/data_structure.cpp
+++ src/data_structure.cpp
@@ -1,4 +1,5 @@
 #include "data_structure.hpp"
+#include <cassert>
 
 // файл с определениями
 
@@ -6,4 +7,87 @@ namespace itis {
 
   // здесь должны быть определения методов вашей структуры
 
+  Circular_buffer::Circular_buffer(int capacity_arg) {
+    assert(capacity_arg > 0);
+    capacity_ = capacity_arg;
+  }
+
+  void Circular_buffer::add(int e) {
+    if(isFull()){
+      addIfFull(e);
+    }else{
+      addIfNotFull(e);
+    }
+  }
+
+  int Circular_buffer::get() {
+    assert(size_ != 0);
+    int data = oldest_node->data;
+    SinglyNode *node = oldest_node;
+    oldest_node = oldest_node->next;
+    delete node;
+    size_--;
+    return data;
+  }
+
+  int Circular_buffer::peek() {
+    assert(size_ != 0);
+    return oldest_node->data;
+  }
+
+  void Circular_buffer::clear() {
+    SinglyNode *node = oldest_node;
+    for (int i = 0; i < size_; ++i) {
+      oldest_node = oldest_node->next;
+      delete node;
+      node = oldest_node;
+    }
+    size_ = 0;
+    capacity_ = 0;
+    oldest_node = nullptr;
+    newest_node = nullptr;
+    delete node;
+  }
+
+  bool Circular_buffer::isFull() {
+    return size_ == capacity_;
+  }
+
+  bool Circular_buffer::isEmpty() {
+    return size_ == 0;
+  }
+
+  int Circular_buffer::currentSize() {
+    return size_;
+  }
+
+  Circular_buffer::~Circular_buffer() {
+    clear();
+  }
+
+  void Circular_buffer::addIfFull(int e) {
+    SinglyNode *node = oldest_node;
+    oldest_node = oldest_node->next;
+    node->data = e;
+    newest_node->next = node;
+    newest_node = node;
+  }
+
+  void Circular_buffer::addIfNotFull(int e) {
+    auto *node = new SinglyNode(e, nullptr);
+    if(isEmpty()){
+      oldest_node = node;
+      newest_node = node;
+    }else{
+      if(size_ == capacity_ - 1){
+        node->next = oldest_node;
+        newest_node->next = node;
+        newest_node = node;
+      }else{
+        newest_node->next = node;
+        newest_node = node;
+      }
+    }
+    size_++;
+  }
 }  // namespace itis
