1d34170653bce4a8816c7e0ee1b8004c4684e017
Emil Davlyatov <emil.davlyatov2014@yandex.ru>
2021-04-18 20:24:52 +0300

    Reworked all project

--- src/data_structure.cpp
+++ src/data_structure.cpp
@@ -1,93 +1,153 @@
 #include "data_structure.hpp"
 #include <cassert>
+#include <algorithm>
 
 // файл с определениями
 
 namespace itis {
 
-  // здесь должны быть определения методов вашей структуры
-
-  Circular_buffer::Circular_buffer(int capacity_arg) {
-    assert(capacity_arg > 0);
-    capacity_ = capacity_arg;
-  }
-
-  void Circular_buffer::add(int e) {
-    if(isFull()){
-      addIfFull(e);
-    }else{
-      addIfNotFull(e);
-    }
-  }
-
-  int Circular_buffer::get() {
-    assert(size_ != 0);
-    int data = oldest_node->data;
-    SinglyNode *node = oldest_node;
-    oldest_node = oldest_node->next;
-    delete node;
-    size_--;
-    return data;
-  }
-
-  int Circular_buffer::peek() {
-    assert(size_ != 0);
-    return oldest_node->data;
-  }
-
   void Circular_buffer::clear() {
-    SinglyNode *node = oldest_node;
-    for (int i = 0; i < size_; ++i) {
-      oldest_node = oldest_node->next;
-      delete node;
-      node = oldest_node;
-    }
+    oldestElem = newestElem = dataArr;
     size_ = 0;
-    capacity_ = 0;
-    oldest_node = nullptr;
-    newest_node = nullptr;
-    delete node;
   }
 
-  bool Circular_buffer::isFull() {
-    return size_ == capacity_;
+  Circular_buffer::Circular_buffer(int capacity) {
+
+    assert(capacity > 0);
+    capacity_ = capacity;
+    dataArr = new int[capacity];
+    oldestElem = newestElem = dataArr;
   }
 
   bool Circular_buffer::isEmpty() {
     return size_ == 0;
   }
 
-  int Circular_buffer::currentSize() {
+  bool Circular_buffer::isFull() {
+    return size_ == capacity_;
+  }
+
+  void Circular_buffer::addFront(int value) {
+    if (isEmpty()) {
+      addToEmpty(value);
+    } else {
+      addFrontToNotEmpty(value);
+    }
+  }
+
+  void Circular_buffer::addBack(int value) {
+    if (isEmpty()) {
+      addToEmpty(value);
+    } else {
+      addBackToNotEmpty(value);
+    }
+  }
+
+  void Circular_buffer::addToEmpty(int value) {
+    dataArr[0] = value;
+    size_++;
+  }
+
+
+  void Circular_buffer::addFrontToNotEmpty(int value) {
+    if(isFull()){
+      *oldestElem = value;
+      newestElem = oldestElem++;
+      if(oldestElem > &dataArr[size_-1]){
+        oldestElem = &dataArr[0];
+      }
+    }else{
+      newestElem = newestElem + 1;
+      if(newestElem > &dataArr[size_ - 1]){
+        newestElem = &dataArr[0];
+      }
+      *newestElem = value;
+      size_++;
+    }
+  }
+
+  void Circular_buffer::addBackToNotEmpty(int value) {
+    if(isFull()){
+      *newestElem = value;
+      oldestElem = newestElem--;
+      if(newestElem < &dataArr[0]){
+        newestElem = &dataArr[size_ -1];
+      }
+    }else{
+      oldestElem = oldestElem - 1;
+      if(oldestElem < &dataArr[0]){
+        oldestElem = &dataArr[size_ - 1];
+      }
+      *oldestElem = value;
+      size_++;
+    }
+  }
+
+  int Circular_buffer::peekFront() {
+    if(size_ == 0){
+      throw "Peeking into an empty array";
+    }
+    return *newestElem;
+  }
+
+  int Circular_buffer::peekBack() {
+    if(size_ == 0){
+      throw "Peeking into an empty array";
+    }
+    return *oldestElem;
+  }
+
+  int Circular_buffer::getSize() {
     return size_;
   }
 
-  Circular_buffer::~Circular_buffer() {
-    clear();
+  void Circular_buffer::expand(int newCapacity) {
+    assert(newCapacity > capacity_);
+    int *newDataArr = new int[newCapacity];
+    int indexOfOldestElem = oldestElem - dataArr;
+    int indexOfNewestElem = newestElem - dataArr;
+    std::copy(dataArr, &dataArr[size_], newDataArr);
+
+    oldestElem = &newDataArr[indexOfOldestElem];
+    newestElem = &newDataArr[indexOfNewestElem];
+
+    delete[] dataArr;
+    dataArr = newDataArr;
   }
 
-  void Circular_buffer::addIfFull(int e) {
-    SinglyNode *node = oldest_node;
-    oldest_node = oldest_node->next;
-    node->data = e;
-    newest_node->next = node;
-    newest_node = node;
+  int Circular_buffer::getCapacity() {
+    return capacity_;
   }
 
-  void Circular_buffer::addIfNotFull(int e) {
-    auto *node = new SinglyNode(e, nullptr);
-    if(isEmpty()){
-      oldest_node = node;
-      newest_node = node;
-    }else{
-      if(size_ == capacity_ - 1){
-        node->next = oldest_node;
-        newest_node->next = node;
-        newest_node = node;
-      }else{
-        newest_node->next = node;
-        newest_node = node;
-      }
+  int Circular_buffer::getBack() {
+    if(size_ == 0){
+      throw "Trying to get elem from empty array";
     }
-    size_++;
+    int elem = *oldestElem;
+    if(++oldestElem > &dataArr[size_ - 1]){
+      oldestElem = dataArr;
+    }
+    size_--;
+    return elem;
   }
+
+  int Circular_buffer::getFront() {
+    if(size_ == 0){
+      throw "Trying to get elem from empty array";
+    }
+    int elem = *newestElem;
+    if(--newestElem < dataArr){
+      newestElem = &dataArr[size_ - 1];
+    }
+    size_--;
+    return elem;
+  }
+
+  Circular_buffer::~Circular_buffer() {
+    delete[] dataArr;
+    oldestElem = newestElem = nullptr;
+    size_ = 0;
+    capacity_ = 0;
+  }
+
 }  // namespace itis
