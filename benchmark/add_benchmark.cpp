37fc2f1a01545402194d379b25adab57774ed75d
Emil Davlyatov <emil.davlyatov2014@yandex.ru>
2021-04-24 22:51:50 +0300

    Fix main in add_benchmark.cpp

--- benchmark/add_benchmark.cpp
+++ benchmark/add_benchmark.cpp
@@ -3,7 +3,6 @@
 #include <string>       // string, stoi
 #include <string_view>  // string_view
 #include <chrono>       // high_resolution_clock, duration_cast, nanoseconds
-#include <sstream>      // stringstream
 #include <vector>
 
 // подключаем вашу структуру данных
@@ -16,18 +15,12 @@ using namespace itis;
 static constexpr auto kDatasetPath = string_view{PROJECT_DATASET_DIR};
 static constexpr auto kProjectPath = string_view{PROJECT_SOURCE_DIR};
 
-long addFront_test(ifstream &stream){
-  string line;
+long addFront_test(vector<int> &vector){
   Circular_buffer *cb = new Circular_buffer();
   float globalTime = 0;
   long inCycleTime = 0;
-  vector<int> vector;
-
-  while (getline(stream, line, ',')){
-    vector.push_back(stoi(line));
-  }
-
   int elem;
+
   for (int i = 0; i < 10; ++i) {
     for (int j = 0; j < vector.size(); ++j) {
       elem = vector[j];
@@ -47,16 +40,10 @@ long addFront_test(ifstream &stream){
   return (long) globalTime/10;
 }
 
-long addBack_test(ifstream &stream){
-  string line;
+long addBack_test(vector<int> &vector){
   Circular_buffer *cb = new Circular_buffer();
   float globalTime = 0;
   long inCycleTime = 0;
-  vector<int> vector;
-
-  while (getline(stream, line, ',')){
-    vector.push_back(stoi(line));
-  }
 
   int elem;
   for (int i = 0; i < 10; ++i) {
@@ -68,7 +55,7 @@ long addBack_test(ifstream &stream){
 
       // переводим время в наносекунды
       const auto time_diff = time_point_after - time_point_before;
-      inCycleTime += chrono::duration_cast<chrono::nanoseconds>(time_diff).count();
+      inCycleTime += (long) chrono::duration_cast<chrono::nanoseconds>(time_diff).count();
     }
     globalTime+=inCycleTime/(1000);
     inCycleTime = 0;
@@ -85,15 +72,22 @@ int main() {
   auto output_file = ofstream (path + "/add_result.csv");
   ifstream input_file;
   string message;
+  string line;
+  vector<int> intVector;
   for (int i = 1; i < 11; ++i) {
     for (int j = 0; j < 10; ++j) {
       input_file = ifstream(path + "/TestCase" + to_string(i) + "/TestData" + to_string(j) + ".csv");
+      while (getline(input_file, line, ',')){
+        intVector.push_back(stoi(line));
+      }
+
       message = "Operation: addFront(),test case " + to_string(i) + "." + to_string(j) + ","
-                + to_string(addFront_test(input_file));
+                + to_string(addFront_test(intVector));
       output_file << message << ",     ,";
       message = "Operation: addBack(),test case " + to_string(i) + "." + to_string(j) + ","
-      + to_string(addBack_test(input_file));
+      + to_string(addBack_test(intVector));
       output_file << message<<endl;
+      intVector.clear();
     }
   }
 
